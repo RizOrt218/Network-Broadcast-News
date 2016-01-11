f4a5139b4a0ecc5d91ceb000279fcf74080ebe29
RizOrt218 <rizhiafalen@gmail.com>
2016-01-11 10:11:17 -1000

    client is able to chat with other clients!

--- client.js
+++ client.js
@@ -1,11 +1,26 @@
-const net = require( 'net' );
-const host = 'localhost';
-const port = 2468;
+const net  = require( 'net' );
+const HOST = 'localhost';
+const PORT = 9999;
 
-const server = net.createServer( (client) => {
-  client.write('Hi there!');
+const server = net.connect({ host: HOST , port: PORT }, () => {
+  console.log( 'connected to server!' );
+  process.stdin.setEncoding( 'utf8' );
+  server.setEncoding( 'utf8' );
+
+  process.stdout.write('ClientASays: ');
+
+  process.stdin.on('data', (data) => {
+    server.write( data );
+  });
+
+  //
+  server.on('data', (data) => {
+    process.stdout.write('\r' + data + '\nClientASays: ');
+  });
+
+  //when server ends connection
+  server.on('end', function () {
+    console.log('end connection')
+  })
+
 });
-
-server.listen(2468, () => {
-  console.log( 'Server is running' );
-})
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-const net  = require( 'net' );
+const net = require( 'net' );
 const host = 'localhost';
-const port = 2468;
+const port = 9999;
 
-const server = net.connect({host: host, port: port}, () => {
-  console.log( 'connected to server!' );
-  server.setEncoding( 'utf8' );
-  process.stdin.setEncoding( 'utf8' );
+var chatters = [];
 
-  process.stdout.write('RizziSays: ');
+const server = net.createServer( (client) => {
 
-  process.stdin.on('data', (data) => {
-    server.write( data );
-  });
+  //what client will get when they get connected
+  client.write('This is your server, Welcome eeerbody');
 
-  server.on('data', (data) => {
-    process.stdout.write('\r' + data + '\nRizzySays: ');
-  });
+  //when a client has been connected
+  process.stdout.write('Client A connected\n');
 
-});
+  //trying to create an array to track who's connecting
+  chatters.push( client );
+  console.log( chatters.length );
+
+  //when client types something it'll return to other clients
+  client.on('data', function (data) {
+    for(var i = 0; i < chatters.length; i++) {
+
+      if( chatters[i] !== client ) {
+        chatters[i].write( data )
+      }
+
+    }
+  })
+
+
+
+  //when client ends connection
+  client.on('end', function () {
+    console.log('client ended connection')
+  })
+
+}).listen(9999, () => {
+  console.log( 'Server is running' );
+})
