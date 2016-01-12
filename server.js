d71a7bfb64a684ad90fc9123fca19d06375f0f03
RizOrt218 <rizhiafalen@gmail.com>
2016-01-11 22:07:38 -1000

    dirty code, need to work on it...

--- server.js
+++ server.js
@@ -3,29 +3,36 @@ const host  = 'localhost';
 const port  = 9999;
 
 var chatters = [];
+var username = null;
 
-const server = net.createServer( (client) => {
-  var username = null;
+const server = net.createServer( (client, username) => {
 
   client.setEncoding( 'utf8' );
   //readable to client when they connect
-  client.write('This is your server, Welcome!\nEnter username: ');
+
+  client.write('This is your server, Welcome!\nEnter username: \r');
 
   //prompts server when a client connected
   process.stdout.write('Connected to '+host+': '+client.remotePort+'\n');
 
   //trying to create an array to track who's connecting
   chatters.push( client );
-  // console.log( chatters );
+  console.log( chatters.onconnection );
   //when client types something it'll return to other clients
   client.on('data', function (data) {
     if( client.hasOwnProperty( 'username' ) === false ) {
       client.username = data.toUpperCase().trim();
       username = client.username;
+      return username;
     }
 
-    for(var i = 0; i < chatters.length; i++) {
-      chatters[i].write( '\r'+ username +': ' + data);
+    for( var i = 0; i < chatters.length; i++ ) {
+      chatters[i].write( data );
+      // if( chatters[i] !== client ) {
+      //   chatters[i].write( username +': ' + data );
+      // } else {
+      //   chatters[i].write( username +': ' + data )
+      // }
     }
   }) // end of client.on
 
